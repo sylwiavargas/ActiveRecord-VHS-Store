05844294d5ec67c54d9ada3b8df9aaaee69f3165
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-20 16:40:32 -0500

    adds serial number

--- app/models/vhs.rb
+++ app/models/vhs.rb
@@ -3,4 +3,20 @@ class Vhs < ActiveRecord::Base
     has_many :rentals
     has_many :clients, through: :rentals
 
+    private
+
+    def long_title?
+        !self.movie.title.nil? && self.movie.title.length > 2
+    end
+
+    def two_part_title?
+        !self.movie.title.split(" ")[1].nil? && self.movie.title.split(" ")[1].length > 2
+    end
+
+    def serial_number_stub
+        return "X" if self.movie.title.nil?
+        return self.movie.title.split(" ")[1][0..3].gsub(/s/, "").upcase + "-" if two_part_title?
+        return self.movie.title.gsub(/s/, "").upcase + "-" unless long_title?
+        self.movie.title[0..3].gsub(/s/, "").upcase + "-"
+    end
 end
